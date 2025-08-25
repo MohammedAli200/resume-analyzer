19368f5ed0878090117365cefb8829df8e90983c
Mohammed Ali <alimohammed3731@gmail.com>
2025-08-25 13:49:32 +0530

    Add files via upload

---
+++ app/routes/home.tsx
@@ -0,0 +1,31 @@
+import Navbar from "~/components/Navbar";
+import type { Route } from "./+types/home";
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
+
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "samrt feedback for your resume!" },
+  ];
+}
+
+export default function Home() {
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI powered feedback</h2>
+      </div>
+    </section>
+    {resumes.length > 0 && (
+      <div className="resume-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume}></ResumeCard>
+        ))}
+      </div>
+    )}
+  </main>
+}
